86f8024c5bb93ad324c4daf5c06a7cb59d240588
saprasa <sameer_sapra@manulife.ca>
2020-07-01 11:04:48 -0400

    test:module2 pass2

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => res.render('index', { title: 'Index' }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
