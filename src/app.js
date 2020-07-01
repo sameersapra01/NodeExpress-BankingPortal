8d66a0bc3ba1b7003d2f95a0a25f21dd2dd3a919
saprasa <sameer_sapra@manulife.ca>
2020-07-01 11:09:17 -0400

    test:module2 pass6

--- src/app.js
+++ src/app.js
@@ -17,5 +17,7 @@ const users = JSON.parse(userData);
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
