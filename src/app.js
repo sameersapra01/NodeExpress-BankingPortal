8f56e41c01e8626e3a8fd2fe463b4a7ba64925a2
saprasa <sameer_sapra@manulife.ca>
2020-07-01 13:30:16 -0400

    test:module4 pass

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const { accounts, users, writeJSON } = require('./data.js');
+
 
 const express = require('express');
 const app = new express();
@@ -9,11 +11,7 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
 
 app.use(express.urlencoded({extended : true}));
 
@@ -33,7 +31,7 @@ app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance -= req.body.amount;
     accounts[req.body.to].balance += parseInt(req.body.amount, 10);
     let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
 
@@ -43,7 +41,7 @@ app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
     let accountsJSON = JSON.stringify(accounts, null, 4)
-    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 
