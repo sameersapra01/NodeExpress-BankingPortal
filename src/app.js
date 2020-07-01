c02fb11aac3ed522d6fbf6e1abaac75a17c491f3
saprasa <sameer_sapra@manulife.ca>
2020-07-01 13:00:01 -0400

    test:module3 pass6

--- src/app.js
+++ src/app.js
@@ -18,10 +18,25 @@ const users = JSON.parse(userData);
 app.use(express.urlencoded({extended : true}));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
+
 app.get('/transfer', (req, res) =>  res.render('transfer'));
+
+app.post('/transfer', (req, res) =>{
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += req.body.amount;
+
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON,'utf-8');
+
+    res.render('transfer', {message: 'Transfer Complete'});
+});
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
